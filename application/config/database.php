fe529b82187c34516d3a8596e43a1025feb24c58
arvinnizar <arvin.nizars@gmail.com>
2015-12-30 06:46:24 +0700

    update

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['username'] = 'keuw';
+$db['default']['password'] = 'keuwubhara20140k3';
 $db['default']['database'] = 'db_keuangan';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
